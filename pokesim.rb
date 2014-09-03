5720be452bc57d0bb593058da1f21d578802fd03
Ben Orchard <thefirstmuffinman@gmail.com>
2014-09-03 15:06:51 +0100

    lots of work on things: remembered about IDs...
    Need to store Pokeymans by ID, not name.

--- pokesim.rb
+++ pokesim.rb
@@ -4,14 +4,56 @@
 #
 
 class Pokemon
-  hp
-  atk
-  def
-  sp_atk
-  sp_def
-  spd
+  @@total_pokemon = 0
+
+  attr_accessor :name
+
+  def initialize(name)
+    if @@total_pokemon == 6
+      puts "You already have 6 Pokémon!"
+    elsif @@total_pokemon > 6
+      puts "ERROR: total Pokémon over 6"
+    else
+      @name = name
+      @id = Random.rand(11)
+      @@total_pokemon += 1
+    end
+  end
+
+  public
+  def rename(name)
+    # Name changing is accessed through a public method rather than directly
+    # changing the variable, so we can restrict certain words (e.g. fuck, shit,
+    # balls :D)
+    if name == "restricted"
+      puts "That name isn't allowed!"
+    elsif name == @name
+      puts "That name isn't different!"
+    else
+      @name = name
+    end
+
+    def release
+      @@total_pokemon -= 1
+      puts "How to delete this Pokémon? Should I keep a list of IDs currently
+            in the party?"
+    end
+
+    def show_id
+      puts "usually you won't ever see this"
+      @id
+    end
+  end
 end
 
-class Torchic
-  extend or include ?
-end
+#class Torchic
+#  extend or include ?
+#end
+
+raehik = Pokemon.new("Raehik")
+puts raehik.name
+raehik.rename("Torchic")
+puts raehik.name
+raehik.rename("restricted")
+puts raehik.name
+puts raehik.show_id
